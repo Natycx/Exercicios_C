ac862d4144fe62414e1e5ad0d5ba0897972caf5d
Natycx <talia_garcia@hotmail.com>
2022-02-12 19:08:49 -0300

    First Comit

---
+++ cmake-build-debug/CMakeFiles/C_parte2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_parte2.dir/main.c.obj"
+  "C_parte2.exe"
+  "C_parte2.exe.manifest"
+  "C_parte2.pdb"
+  "libC_parte2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_parte2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

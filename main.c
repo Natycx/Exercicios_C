ccf920309934fe9ed400cf9201a0e3d10ec61a6d
Natycx <talia_garcia@hotmail.com>
2022-02-14 09:41:53 -0300

    Continuação

--- main.c
+++ main.c
@@ -48,6 +48,72 @@ int desafio2370(){
 }
 
 
+int fatorial(int a){
+    if(a == 0){
+        return 1;
+    }else{
+        return a * fatorial(a - 1);
+    }
+}
+
+int desafio1153()
+{
+    int n;
+    scanf("%d", &n);
+
+    printf("%d\n", fatorial(n));
+
+    return 0;
+}
+
+// 0! = 1
+// n! = n * (n-1)!
+
+// fib(0) = 0, fib(1) = 1
+// fib(n) = fib(n-1) + fib(n-2)
+
+int fib_rec(int n) {
+    if (n == 0 || n == 1) // (n <= 1)
+        return n;
+
+    return fib_rec(n-1) + fib_rec(n-2);
+}
+
+//0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, ..., fib(20)
+
+// 0, 1
+// 1, 0+1=1
+// 1, 1+1 = 2
+// 2, 2+1 = 3
+// 3, 2+3 = 5
+// 5, 3+5 = 8
+// ...
+
+typedef unsigned long long ull;
+
+ull fib_loop(int n) {
+    ull v[n+1];
+
+    v[0] = 0;
+    v[1] = 1;
+
+    for(int i = 2; i <= n; ++i)
+        v[i] = v[i-1] + v[i-2];
+
+    return v[n];
+}
+
+int aula14_02() {
+    int x;
+    scanf("%d", &x);
+
+    ull y = fib_loop(x);
+    printf("%llu\n", y);
+
+    return 0;
+}
+
+
 int main() {
     aula02_02();
 
